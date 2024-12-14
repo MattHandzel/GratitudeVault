c0b6f8e002f80bfc2ae22f1242ba3b223999b4c0
MattHandzel <matthewwalkosz@gmail.com>
2024-12-13 21:32:21 -0600

    i had to refresh my cash

--- app/api/public-gratitude/[public_url]/route.ts
+++ app/api/public-gratitude/[public_url]/route.ts
@@ -21,7 +21,7 @@ export async function GET(
     const publicGratitudes = user.gratitudes.filter(
       (gratitude: any) => gratitude.privacyLevel === "public",
     );
-    console.log("api public gratitudes", publicGratitudes);
+    //console.log("api public gratitudes", publicGratitudes);
 
     return NextResponse.json({ gratitudes: publicGratitudes }, { status: 200 });
   } catch (error) {
